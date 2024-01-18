a0375bae309dd6dc913e6e9fa102df1a981b3045
Shivam <shivamaher100@gmail.com>
2024-01-18 10:00:58 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function inputvalue() {
+  const input = document.getElementsByClassName("taskinput");
+  const inputValue = input[0].value;
+  console.log(inputValue);
+  
+
+}
+function taskfly()
+{
+
+
+}
